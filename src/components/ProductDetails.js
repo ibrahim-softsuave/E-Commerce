c6f9effbca083f5b435e06af1e7ef84bac4cea3c
ibrahim-softsuave <ibrahim.softsuave@gmail.com>
2023-03-30 20:26:32 +0530

    same 11 day

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -40,109 +40,111 @@ const ProductDetails = () => {
   }
   return (
     <>
-    <div className='products'>
-      <div className='product-details-container'>
-        <div className='pdi-items pdi-item1'>
-          {productImages.map((item, index) => {
+      <div className='products'>
+        <div className='product-details-container'>
+          <div className='pdi-items pdi-item1'>
+            {productImages.map((item, index) => {
+              return (
+                <img
+                  className='pdi-images'
+                  src={'https://unipick-ui.s3.ap-south-1.amazonaws.com/' + item}
+                  alt='product multiple images'
+                  onClick={handleImage}
+                ></img>
+              )
+            })
+            }
+          </div>
+          <div className='pdi-items pdi-item2'>
+            <img className='pdi-full-image' src={fullsSizeImage || 'https://unipick-ui.s3.ap-south-1.amazonaws.com/' + productImages[0]} alt='Full SizeImage'></img>
+          </div>
+          <div className='pdi-items pdi-item3'><div className='prod-image-share'><BSIcons.BsFillShareFill /></div></div>
+        </div>
+        <div className='product-details'>
+          {item.map(things => {
             return (
-              <img
-                className='pdi-images'
-                src={'https://unipick-ui.s3.ap-south-1.amazonaws.com/' + item}
-                alt='product multiple images'
-                onClick={handleImage}
-              ></img>
-            )
-          })
-          }
-        </div>
-        <div className='pdi-items pdi-item2'>
-          <img className='pdi-full-image' src={fullsSizeImage || 'https://unipick-ui.s3.ap-south-1.amazonaws.com/' + productImages[0]} alt='Full SizeImage'></img>
-        </div>
-        <div className='pdi-items pdi-item3'><div className='prod-image-share'><BSIcons.BsFillShareFill /></div></div>
-      </div>
-      <div className='product-details'>
-        {item.map(things => {
-          return (
-            <>
-              <p>{things.brandName}</p>
-              <h4>{things.productName}</h4>
-                <div>
+              <>
+                <p>{things.brandName}</p>
+                <h4>{things.productName}</h4>
+                <div className='pd-item1'>
                   <div className='counter'>
                     <button value='decrease' onClick={handleProductCount}>-</button>
                     <p>{productCount}</p>
                     <button value='increase' onClick={handleProductCount}>+</button>
-                </div>
+                  </div>
                   <p>{things.productVariant[0].price}</p>
                   <p>Local tax included (where applicable)</p>
                   <p>In stock</p>
                   <div>
+                    <div>
                     <span>image</span>
                     <p>Order within next 23 hours 9 min to get it by April 1</p>
+                    </div>
                     <p>shipping charges will be calculated on checkout</p>
                   </div>
-              </div>
-              <div>
-                <button>Add to card</button>
-              </div>
-              <div>
-                <button>Add to collection</button>
-              </div>
-              <p>only{things.totalQuantity}left and over 15 people have it in their carts.</p>
-              <div className='Additional-content'>
-                <div className='seller' id='seller' onClick={handleShowContent}>
-                  <p>Meet & interact with your Seller</p>
-                  <p>+</p>
                 </div>
-                <div className={showContent === 'seller' ? 'seller-content show-additional-content' : 'seller-content'}>
-                  <div></div>
-                  <div className='sub-seller-content'>
-                    <p>
-                      Following
-                    </p>
-                    <p>
-                      Followers
-                    </p>
+                <div>
+                  <button>Add to card</button>
+                </div>
+                <div>
+                  <button>Add to collection</button>
+                </div>
+                <p>only{things.totalQuantity}left and over 15 people have it in their carts.</p>
+                <div className='Additional-content'>
+                  <div className='seller' id='seller' onClick={handleShowContent}>
+                    <p>Meet & interact with your Seller</p>
+                    <p>+</p>
                   </div>
-                </div>
-              </div>
-              <div className='Additional-content'>
-                <div className='description' id='description' onClick={handleShowContent}>
-                  <p>Description</p>  <p>+</p>
-                </div>
-                <div className={showContent === 'description' ? 'description-content show-additional-content' : 'description-content'}>
-                  {things.description}
-                </div>
-              </div >
-              <div className='Additional-content'>
-                <div className='attributes' id='attributes' onClick={handleShowContent}>
-                  <p> Attributes</p> <p>+</p>
-                </div>
-                <div className={showContent === 'attribute' ? 'attribute-content show-additional-content' : 'attribute-content'}></div>
-              </div>
-              <div className='Additional-content'>
-                <div className='return-policies' id='return-policies' onClick={handleShowContent}>
-                  <p>Return Policies</p><p>+</p>
-                </div>
-                <div className={showContent === 'return-policies' ? 'return-policies-content show-additional-content' : 'return-policies-content'}>
-                  <p>Cancel before shipping, Return within 7 days of delivery for exchange or refund.</p>
-                  <div className='sub-return-policies-content'>
-                    <div>
-                      <p>Returns & Exchanges</p>
-                      <p>Accepted</p>
-                      <p>Exception may apply</p>
+                  <div className={showContent === 'seller' ? 'seller-content show-additional-content' : 'seller-content'}>
+                    <div></div>
+                    <div className='sub-seller-content'>
+                      <p>
+                        Following
+                      </p>
+                      <p>
+                        Followers
+                      </p>
                     </div>
-                    <p>
-                      Return ,Refund and Replacement Policy
-                    </p>
                   </div>
                 </div>
-              </div>
-            </>
-          )
-        })}
+                <div className='Additional-content'>
+                  <div className='description' id='description' onClick={handleShowContent}>
+                    <p>Description</p>  <p>+</p>
+                  </div>
+                  <div className={showContent === 'description' ? 'description-content show-additional-content' : 'description-content'}>
+                    {things.description}
+                  </div>
+                </div >
+                <div className='Additional-content'>
+                  <div className='attributes' id='attributes' onClick={handleShowContent}>
+                    <p> Attributes</p> <p>+</p>
+                  </div>
+                  <div className={showContent === 'attribute' ? 'attribute-content show-additional-content' : 'attribute-content'}></div>
+                </div>
+                <div className='Additional-content'>
+                  <div className='return-policies' id='return-policies' onClick={handleShowContent}>
+                    <p>Return Policies</p><p>+</p>
+                  </div>
+                  <div className={showContent === 'return-policies' ? 'return-policies-content show-additional-content' : 'return-policies-content'}>
+                    <p>Cancel before shipping, Return within 7 days of delivery for exchange or refund.</p>
+                    <div className='sub-return-policies-content'>
+                      <div>
+                        <p>Returns & Exchanges</p>
+                        <p>Accepted</p>
+                        <p>Exception may apply</p>
+                      </div>
+                      <p>
+                        Return ,Refund and Replacement Policy
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )
+          })}
 
+        </div>
       </div>
-    </div>
     </>
   )
 }
